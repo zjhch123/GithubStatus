42cd490a01e252b1d4ba0e41fe705f0b3a448c7f
zjhch123 <416836508@qq.com>
2017-06-29 16:17:58 +0800

    .

--- GithubStatus/StatusMenuController.swift
+++ GithubStatus/StatusMenuController.swift
@@ -9,9 +9,10 @@
 import Cocoa
 
 
-class StatusMenuController: NSObject, GithubRequestDelegate {
+class StatusMenuController: NSObject, GithubRequestDelegate, PreferencesWindowDelegate {
 
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    let DEFAULT_USER = "octocat"
     
     var githubRequest: GithubRequest!
     
@@ -19,13 +20,32 @@ class StatusMenuController: NSObject, GithubRequestDelegate {
     @IBOutlet weak var statusView: StatusView!
     var statusMenuItem: NSMenuItem!
 
+    var preferencesWindow: PreferencesWindow!
+    
+    func getDefaultUser() -> String {
+        let defaults = UserDefaults.standard
+        let user = defaults.string(forKey: "username") ?? DEFAULT_USER
+        return user
+    }
+    
+    func updateCount() {
+        let user = getDefaultUser()
+        githubRequest.request(username: user)
+    }
+    
     func githubRequestDidUpdate(username: String, count: String?) {
         if let _count = count {
             statusView.update(username: username, count: _count)
         }
     }
     
+    func preferencesDisUpdate() {
+        updateCount()
+    }
+    
     override func awakeFromNib() {
+        preferencesWindow = PreferencesWindow()
+        preferencesWindow.delegate = self
         githubRequest = GithubRequest(delegate: self)
         statusItem.menu = statusMenu
         let icon = NSImage(named: "statusIcon")
@@ -34,14 +54,21 @@ class StatusMenuController: NSObject, GithubRequestDelegate {
         statusItem.menu = statusMenu
         statusMenuItem = statusMenu.item(withTitle: "Status")
         statusMenuItem.view = statusView
+        statusView.usernameTextField.stringValue = getDefaultUser()
+        updateCount();
     }
+    
     
     @IBAction func quitClicked(_ sender: AnyObject) {
         NSApplication.shared().terminate(self)
     }
     
     @IBAction func updateClicked(_ sender: AnyObject) {
-        githubRequest.request(username: "zjhch123");
+        updateCount()
+        
     }
-    
+
+    @IBAction func preferencesClicked(_ sender: AnyObject) {
+        preferencesWindow.showWindow(nil)
+    }
 }
